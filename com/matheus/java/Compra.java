240115105ead458e71a6389baa5be377e4f184ab
Matheus Sampaio <matheuslemos98@gmail.com>
2022-08-18 16:01:42 -0300

    Construção do Menu e da classe de Compra

---
+++ com/matheus/java/Compra.java
@@ -0,0 +1,5 @@
+package com.matheus.java;
+
+public class Compra {
+
+}
--- com/matheus/java/Faturamento.java
+++ com/matheus/java/Faturamento.java
@@ -1,9 +1,38 @@
 package com.matheus.java;
 
+import java.util.Scanner;
+
 public class Faturamento {
 
     public static void main(String[] args) {
 
+        int optMenu, qtdItem;
+        String nomeItem;
+
+
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("******** Sistema de Compras ********");
+        System.out.println(" 1 - Compra de Produto");
+        System.out.println(" 2 - Consultar saldo");
+        System.out.println(" 3 - Sair");
+        optMenu = scan.nextInt();
+
+
+            switch (optMenu){
+                case 1:
+                    System.out.println("******** Compra ********");
+                    System.out.println("");
+                    System.out.println("Digite o nome do Item: ");
+
+                    break;
+                case 2:
+                    System.out.println("******** Saldo ********");
+                    break;
+                case 3:
+                    System.out.println("Obrigado pela preferência");
+                    break;
+                }
     }
 
 }
